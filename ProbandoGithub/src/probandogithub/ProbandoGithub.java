d231a1e456f596d27976f27d92aacf866d22642b
Eduardo <Eduardo@Ed>
2016-03-31 09:54:24 +0200

    Primera prueba de github

---
+++ ProbandoGithub/src/probandogithub/ProbandoGithub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probandogithub;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class ProbandoGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 6;
+        String b = "mar";
+        char c = 't';
+
+        System.out.println("Mi número favorito es el " + a + " mi día favorito es el " + b + "tes y la letra la " + c);
+    }
+
+}
